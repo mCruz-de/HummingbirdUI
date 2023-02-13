6b6637779ea7526ab1bfc21c67c7d42035d521c2
Maurice Kreutz <maurice.kreutz@m-cruz.de>
2023-02-13 14:53:51 +0100

    HummingbirdUI v4.0.0 start

---
+++ hummingbird-ui/hummingbird-ui.js
@@ -0,0 +1,38 @@
+/*!
+  * HummingbirdUI v4.0.0 (https://hummingbird-ui.net/)
+  * Copyright 2023 HummingbirdUI
+  * Licensed under MIT (https://github.com/mCruz-de/HummingbirdUI/blob/main/LICENSE)
+  */
+
+
+let breakpoint_mobile = 1080;
+
+let overlay = document.createElement('div');
+overlay.id = "overlay";
+overlay.setAttribute('onclick','closeSidenav()');
+
+
+function toggleSidenav(){
+    document.body.classList.toggle('toggle-sidenav');
+    if (window.innerWidth < breakpoint_mobile) {
+      document.body.appendChild(overlay);
+    }
+}
+
+function closeSidenav(){
+  document.body.classList.toggle('toggle-sidenav');
+  document.body.removeChild(overlay);
+}
+
+window.addEventListener("resize", function() {
+  if (window.innerWidth < breakpoint_mobile) {
+    document.body.classList.remove("toggle-sidenav");
+    document.body.removeChild(overlay);
+  }
+});
+
+
+/* dark mode */
+function toggleDarkMode(){
+  document.body.classList.toggle('dark-mode');
+}
